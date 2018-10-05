28bf98e5add8ab9ccad756fd1c471bad6c2f9ebd
davidmgrantham <davidmgranhtam@gmail.com>
2018-10-05 17:48:21 -0500

    Added abs.py

---
+++ Built-In_Functions/abs.py
@@ -0,0 +1,32 @@
+# abs(Absulute Values)
+
+print("An absulute value in math means simply how many numbers it is from 0.")
+
+print('') # Just for spacing in output :D
+
+print("=====Number Line=====")
+print("-- -- -- -- -- -- -- -- -- -- -- -- --  --  -- -- -- --")
+print("  -10                            0")
+
+print('') # Just for spacing in output :D
+
+print("=====Answer=====")
+print("It's 10 numbes from 0... -1 -2 -3 -4 -5 -6 -7 -8 -8 -10: Returns:", abs(-10))
+print("Absulute values will always represent positive numbers")
+
+print('') # Just for spacing in output :D
+
+print("=====Comparison Operators=====")
+print("abs(-5) == abs(5)")
+print(abs(-5) == abs(5))
+
+print('') # Just for spacing in output :D
+
+print("abs(8) >= abs(-8)")
+print(abs(8) >= abs(-8))
+
+print('') # Just for spacing in output :D
+
+print("abs(-5) < abs(-15)")
+print(abs(-5) < abs(-15))
+
